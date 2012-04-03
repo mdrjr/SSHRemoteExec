7171a3d7a5175dee5f9df9710b04eafa05b6b7d0
Mauro Ribeiro <git@mdrjr.net>
2012-04-03 11:49:43 -0300

    criado activitys e layouts para list e delete

--- gen/net/mdrjr/sshremoteExec/R.java
+++ gen/net/mdrjr/sshremoteExec/R.java
@@ -56,7 +56,9 @@ public final class R {
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int mainmenu_command_add=0x7f030001;
-        public static final int mainmenu_server_add=0x7f030002;
+        public static final int mainmenu_command_list=0x7f030002;
+        public static final int mainmenu_server_add=0x7f030003;
+        public static final int mainmenu_server_list=0x7f030004;
     }
     public static final class menu {
         public static final int mainmenu=0x7f050000;
