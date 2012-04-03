e719bc16f70e646383d2ddbc487d5d90434990de
Mauro Ribeiro <git@mdrjr.net>
2012-04-03 11:49:48 -0300

    criado activitys e layouts para list e delete

---
+++ src/net/mdrjr/sshremoteExec/MenuCommandsListActivity.java
@@ -0,0 +1,12 @@
+package net.mdrjr.sshremoteExec;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuCommandsListActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainmenu_command_list);
+	}
+}
---
+++ src/net/mdrjr/sshremoteExec/MenuSeversListActivity.java
@@ -0,0 +1,12 @@
+package net.mdrjr.sshremoteExec;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuSeversListActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainmenu_server_list);
+	}
+}
