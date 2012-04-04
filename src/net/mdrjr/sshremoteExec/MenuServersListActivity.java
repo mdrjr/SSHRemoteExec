3b55ebcc1093862837907901216749697249341e
Mauro Ribeiro <git@mdrjr.net>
2012-04-04 11:11:19 -0300

    Mais XML ...

--- src/net/mdrjr/sshremoteExec/MenuServersListActivity.java
+++ src/net/mdrjr/sshremoteExec/MenuServersListActivity.java
@@ -1,12 +1,16 @@
 package net.mdrjr.sshremoteExec;
 
+import net.mdrjr.sshremoteExec.db.DAO.ServerDAO;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class MenuServersListActivity extends Activity {
+	private ServerDAO sDao;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mainmenu_server_list);
+		sDao = new ServerDAO(this);
 	}
 }
